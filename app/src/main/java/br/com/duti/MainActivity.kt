1d447c7c26ec71639b13adce2cf2bc9a53ca22c9
Giovanni dos Santos <rm550303@fiap.com.br>
2024-03-04 23:10:17 -0300

    Criação do layout superior (sem a implementação da busca)

--- app/src/main/java/br/com/duti/MainActivity.kt
+++ app/src/main/java/br/com/duti/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -45,4 +46,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
