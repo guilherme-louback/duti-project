a159da8e856df982b9fd673454bbdc37b72b60e8
Guilherme Louback <luisguilhermels21@gmail.com>
2024-03-07 22:47:52 -0300

    Modificações na MainScreen

--- app/src/main/java/br/com/duti/screens/MainScreen.kt
+++ app/src/main/java/br/com/duti/screens/MainScreen.kt
@@ -1,61 +1,31 @@
 package br.com.duti.screens
 
-import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import br.com.duti.R
 
 @Composable
 fun MainScreen(navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Box(
-            modifier = Modifier
-                .background(color = Color.Cyan)
-                .weight(1f),
-            contentAlignment = Alignment.TopCenter
-        ) {
-            Row(
-                modifier = Modifier.background(color = Color.White)
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.round_search_24),
-                    contentDescription = "Menu Search"
-                )
-            }
-        }
-        Box(
-            modifier = Modifier
-                .background(color = Color.Cyan)
-                .weight(1f),
-            contentAlignment = Alignment.BottomEnd
-        ) {
+    Scaffold(
+        topBar = {
+            TopBar()
+        },
+        bottomBar = {
             Row(
                 horizontalArrangement = Arrangement.SpaceAround,
                 verticalAlignment = Alignment.CenterVertically,
@@ -83,7 +53,36 @@ fun MainScreen(navController: NavController) {
                     contentDescription = "User Icon",
                 )
             }
+        },
+
+        ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+
+            //CONTEÚDO PRINCIPAL
+
         }
+    }
+}
+
+@Composable
+fun TopBar() {
+    Row(
+        modifier = Modifier
+            .background(color = Color.White)
+            .fillMaxWidth()
+            .height(50.dp)
+            .padding(start = 16.dp, end = 16.dp),
+        horizontalArrangement = Arrangement.End,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.round_search_24),
+            contentDescription = "Menu Search"
+        )
     }
 }
 
